b74d85382ee26b5e348c0725fb8ad500f6263687
Aaron Porter <aaron.w.porter@gmail.com>
2020-09-18 22:08:39 -0700

    Add ng-bootstrap and update angular-cli; create task-list component

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
