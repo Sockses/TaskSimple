65ef7fb38e57fc1f4e4f379170b9bb3b142de851
Aaron Porter <aaron.w.porter@gmail.com>
2020-09-19 13:21:47 -0700

    Populate only two dummy task entries, one uncompleted and the other completed

--- Express/routes/task.routes.js
+++ Express/routes/task.routes.js
@@ -3,22 +3,56 @@ const router = express.Router();
 
 const Task = require('../schema/task.schema.js'); //import Task model
 
-var dummyTask = new Task({
-  title: 'Dummy task',
-  completed: false
+Task.deleteMany({ title: /Dummy task/ }, (err) => {
+  console.error(err);
 });
 
-dummyTask.save(function (err, dummyTask) {
-  (err)=>console.error(err);
-});
+var dummyTasks = [
+  {
+    title: 'Dummy task',
+    completed: false
+  },
+  {
+    title: 'Dummy task 2',
+    completed: true,
+    completed_at: Date.now()
+  }
+];
+
+for (let task = 0; task < dummyTasks.length; task++) {
+  let new_task = new Task ({
+    title: dummyTasks[task].title,
+    completed: dummyTasks[task].completed
+  });
+
+  if (dummyTasks[task].completed_at) {
+    new_task.completed_at = dummyTasks[task].completed_at;
+  }
+
+  new_task.save((err) => {
+    console.error(err);
+  });
+}
 
 router.get('/', function(req, res) {
-  Task.findOne().then(
-    documents => {
+  Task.find({ title: /Dummy task/ }).then(
+    (documents) => {
       console.log(documents);
       res.render('index', {
-        title: documents.title,
-        completed: documents.completed
+        document1 : {
+          title: documents[0].title,
+          completed: documents[0].completed,
+          created_at: documents[0].created_at,
+          updated_at: documents[0].updated_at,
+          completed_at: documents[0].completed_at
+        },
+        document2 : {
+          title: documents[1].title,
+          completed: documents[1].completed,
+          created_at: documents[1].created_at,
+          updated_at: documents[1].updated_at,
+          completed_at: documents[1].completed_at
+        }
       });
     }
   );
