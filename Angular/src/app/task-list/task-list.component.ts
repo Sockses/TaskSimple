cd8220c97cae38733772699f0be8dd0499b21d76
Aaron Porter <aaron.w.porter@gmail.com>
2020-10-18 16:16:47 -0700

    Remove Angular default landing content; use TaskService to grab tasks

--- Angular/src/app/task-list/task-list.component.ts
+++ Angular/src/app/task-list/task-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TasksService } from './../services/tasks.service';
+
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.scss']
+  styleUrls: ['./task-list.component.scss'],
 })
 export class TaskListComponent implements OnInit {
-
-  constructor() { }
+  constructor(private tasksService: TasksService) {}
 
   ngOnInit(): void {
+    this.tasksService.getTasks().subscribe((tasksReturned) => {
+      console.log(tasksReturned);
+    });
   }
-
 }
