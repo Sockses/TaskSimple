2e854e72f574813321d32c282ade440bfe135b4c
Aaron Porter <aaron.w.porter@gmail.com>
2021-03-01 08:30:32 -0800

    Initial task-detail component

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TaskComponent } from './task/task.component';
-import { TaskListComponent } from './task-list/task-list.component';
-import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TaskComponent } from "./task/task.component";
+import { TaskListComponent } from "./task-list/task-list.component";
+import { HeaderComponent } from "./header/header.component";
+import { TaskDetailComponent } from "./task-detail/task-detail/task-detail.component";
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { HeaderComponent } from './header/header.component';
     TaskComponent,
     TaskListComponent,
     HeaderComponent,
+    TaskDetailComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
---
+++ Angular/src/app/task-detail/task-detail/task-detail.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+import { Task } from "src/app/models/task.model";
+import { TasksService } from "src/app/services/tasks.service";
+
+@Component({
+  selector: "app-task-detail",
+  templateUrl: "./task-detail.component.html",
+  styleUrls: ["./task-detail.component.scss"],
+})
+export class TaskDetailComponent implements OnInit {
+  task: Task;
+
+  constructor(private taskService: TasksService) {}
+
+  ngOnInit(): void {
+    this.taskService.taskSelected.subscribe((task: Task) => {
+      this.task = task;
+    });
+  }
+}
