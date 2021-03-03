ee8b3c77446360bf0ddb9e325a8d7c9ea6d28f6b
Aaron Porter <aaron.w.porter@gmail.com>
2021-03-03 07:49:41 -0800

    Create User schema

---
+++ Express/schema/user.schema.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
+
+const userSchema = new mongoose.Schema(
+  {
+    uid: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
