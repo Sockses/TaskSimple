458dfbd041bc8ff383f75cef0c77514ff0b4d9b7
Aaron Porter <aaron.w.porter@gmail.com>
2021-03-04 07:36:02 -0800

    Remove Express server code and update/fix  property tests

--- Express/test/task.test.js
+++ Express/test/task.test.js
@@ -1,49 +1,7 @@
-const express = require("express");
 const expect = require("chai").expect;
-const request = require("supertest");
 const Task = require("../schema/task.schema");
 
-function createApp() {
-  const app = express();
-  const router = express.Router();
-
-  const dummy_tasks = [
-    new Task({
-      uid: 1,
-      task_id: 1,
-      title: "Dummy task",
-      completed: false,
-    }),
-    new Task({
-      uid: 2,
-      task_id: 2,
-      title: "Dummy task 2",
-      completed: true,
-      completed_at: Date.now(),
-    }),
-  ];
-
-  router.route("/tasks").get((req, res) => {
-    return res.json(dummy_tasks);
-  });
-
-  app.use(router);
-  return app;
-}
-
 describe("Task", function () {
-  let app;
-
-  before((done) => {
-    app = createApp();
-    app.listen((err) => {
-      if (err) {
-        return done(err);
-      }
-      done();
-    });
-  });
-
   describe("Task Properties", function () {
     it("should be invalid if UID is missing", function (done) {
       new_task = new Task();
@@ -93,15 +51,15 @@ describe("Task", function () {
       });
     });
 
-    it("should be invalid if completed is not a boolean", function (done) {
-      new_task = new Task({ completed: "false" });
+    it("should be invalid if completed is not a boolean or cannot be cast as a boolean", function (done) {
+      new_task = new Task({ completed: "Very false" });
       new_task.validate((err) => {
         expect(err.errors.completed).to.exist;
         done();
       });
     });
 
-    it("have a default completed value of false", function (done) {
+    it("should have a default completed value of false", function (done) {
       new_task = new Task({
         uid: 1,
         task_id: 1,
