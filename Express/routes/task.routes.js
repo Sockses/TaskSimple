8bcbccd6792039eec6b6cc16946886a0626cb069
Aaron Porter <aaron.w.porter@gmail.com>
2021-03-04 14:06:28 -0800

    Add check for no result and catch any errors

--- Express/routes/task.routes.js
+++ Express/routes/task.routes.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const router = express.Router();
-
 const Task = require("../schema/task.schema");
 
-router.get("/", (req, res) => {
-  Task.find({ title: /Dummy task/ }).then((documents) => {
-    console.log(documents);
-    res.append("Access-Control-Allow-Origin", "*");
-    res.json(documents);
-  });
+/**
+ * Get all tasks
+ */
+
+router.get("/", (req, res, next) => {
+  Task.find({})
+    .then((documents) => {
+      if (documents.length == 0) {
+        throw new Error("No documents found");
+      }
+      res.append("Access-Control-Allow-Origin", "*");
+      res.json(documents);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
