699f6ba5e58d591f4352bb455826163c9b679e92
Aaron Porter <aaron.w.porter@gmail.com>
2021-03-06 09:12:01 -0800

    Add cors; add route to add task

--- Express/routes/task.routes.js
+++ Express/routes/task.routes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Task = require("../schema/task.schema");
+const cors = require("cors");
+
+router.use(cors());
 
 /**
  * Get all tasks
@@ -12,8 +15,23 @@ router.get("/", (req, res, next) => {
       if (documents.length == 0) {
         throw new Error("No documents found");
       }
-      res.append("Access-Control-Allow-Origin", "*");
       res.json(documents);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+/**
+ * Add new task
+ */
+
+router.post("/add", (req, res, next) => {
+  const new_task = new Task(req.body.task);
+  new_task
+    .save()
+    .then((document) => {
+      res.json(document);
     })
     .catch((err) => {
       next(err);
