856405bc539726c3a2ccc15b4d082f5929aa9914
Aaron Porter <aaron.w.porter@gmail.com>
2021-03-06 10:25:33 -0800

    Add back Mongo's _id

--- Angular/src/app/models/task.model.ts
+++ Angular/src/app/models/task.model.ts
@@ -5,6 +5,7 @@ export class Task {
     public created_at: string,
     public updated_at: string,
     public completed: boolean,
-    public completed_at?: string
+    public completed_at?: string,
+    public _id?: number
   ) {}
 }
