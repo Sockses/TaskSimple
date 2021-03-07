ac117937ff9a79045277e8c35f136a762bae8e41
Aaron Porter <aaron.w.porter@gmail.com>
2021-03-07 13:20:42 -0800

    Remove duplicate nested folder

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from "./app.component";
 import { TaskComponent } from "./task/task.component";
 import { TaskListComponent } from "./task-list/task-list.component";
 import { HeaderComponent } from "./header/header.component";
-import { TaskDetailComponent } from "./task-detail/task-detail/task-detail.component";
+import { TaskDetailComponent } from "./task-detail/task-detail.component";
 
 @NgModule({
   declarations: [
