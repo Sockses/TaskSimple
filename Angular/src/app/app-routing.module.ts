d8a07c6df72db58392cf3c2f14384c8cbfb36de1
Aaron Porter <aaron.w.porter@gmail.com>
2021-04-22 07:54:19 -0700

    Create login component and form

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./auth/login/login.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 
 const routes: Routes = [
   { path: "", component: DashboardComponent, pathMatch: "full" },
+  { path: "login", component: LoginComponent },
   { path: "not-found", component: NotFoundComponent },
   { path: "**", redirectTo: "not-found" },
 ];
