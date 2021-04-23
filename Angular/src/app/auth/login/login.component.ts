cc4b1b606f62d788958d936edcb741fa49a0144b
Aaron Porter <aaron.w.porter@gmail.com>
2021-04-23 08:06:14 -0700

    Skeleton to handle the login response

--- Angular/src/app/auth/login/login.component.ts
+++ Angular/src/app/auth/login/login.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
+  constructor(public auth: AngularFireAuth, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login(form: NgForm) {
+    this.auth
+      .signInWithEmailAndPassword(form.value.email, form.value.password)
+      .then((userCredential) => {
+        //
+        // Successful login
+        // Do something with the userCredential
+        //
+        this.router.navigate(["/"]);
+      })
+      .catch((error) => {
+        // Notify user of error
+      });
   }
-
 }
