646fa352843d9088da5c83f734144f2c34263d2e
Aaron Porter <aaron.w.porter@gmail.com>
2021-06-09 07:50:10 -0700

    Add tests for signup success and failure

--- Angular/src/app/auth/signup/signup.component.spec.ts
+++ Angular/src/app/auth/signup/signup.component.spec.ts
@@ -1,16 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { FormsModule, NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 
-import { SignupComponent } from './signup.component';
+import { SignupComponent } from "./signup.component";
 
-describe('SignupComponent', () => {
+describe("SignupComponent", () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
 
+  let angularFireAuthMock = jasmine.createSpyObj("AngularFireAuth", [
+    "createUserWithEmailAndPassword",
+  ]);
+  let routerMock = jasmine.createSpyObj("Router", ["navigate"]);
+
+  let form = <NgForm>{
+    value: {
+      email: "",
+      password: "",
+    },
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignupComponent ]
-    })
-    .compileComponents();
+      declarations: [SignupComponent],
+      providers: [
+        { provide: AngularFireAuth, useValue: angularFireAuthMock },
+        { provide: Router, useValue: routerMock },
+      ],
+      imports: [FormsModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +38,34 @@ describe('SignupComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it(
+    "should tell Router to navigate to '/user/dashboard' when signup is successful",
+    waitForAsync(() => {
+      angularFireAuthMock.createUserWithEmailAndPassword.and.returnValue(
+        Promise.resolve()
+      );
+      component.signup(form);
+      fixture.whenStable().then(() => {
+        expect(routerMock.navigate as jasmine.Spy).toHaveBeenCalledWith([
+          "/user/dashboard",
+        ]);
+      });
+    })
+  );
+
+  it(
+    "should display an error message when signup is unsuccessful",
+    waitForAsync(() => {
+      angularFireAuthMock.createUserWithEmailAndPassword.and.returnValue(
+        Promise.reject({ message: "Error" })
+      );
+      component.signup(form);
+      fixture.whenStable().then(() => {
+        fixture.detectChanges();
+        expect(
+          fixture.nativeElement.querySelector("div.alert").textContent.trim()
+        ).toBe("Error");
+      });
+    })
+  );
 });
