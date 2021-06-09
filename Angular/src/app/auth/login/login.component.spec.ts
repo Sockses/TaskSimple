e441a1617b5b198c74b6660fd0a3595e16c7d02d
Aaron Porter <aaron.w.porter@gmail.com>
2021-06-09 07:52:17 -0700

    Use mock naming convention; reorder setup

--- Angular/src/app/auth/login/login.component.spec.ts
+++ Angular/src/app/auth/login/login.component.spec.ts
@@ -8,18 +8,25 @@ import { LoginComponent } from "./login.component";
 describe("LoginComponent", () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-  let form: NgForm;
-  let auth = jasmine.createSpyObj("AngularFireAuth", [
+
+  let angularFireAuthMock = jasmine.createSpyObj("AngularFireAuth", [
     "signInWithEmailAndPassword",
   ]);
-  let router = jasmine.createSpyObj("Router", ["navigate"]);
+  let routerMock = jasmine.createSpyObj("Router", ["navigate"]);
+
+  let form = <NgForm>{
+    value: {
+      email: "",
+      password: "",
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
       providers: [
-        { provide: AngularFireAuth, useValue: auth },
-        { provide: Router, useValue: router },
+        { provide: AngularFireAuth, useValue: angularFireAuthMock },
+        { provide: Router, useValue: routerMock },
       ],
       imports: [FormsModule],
     }).compileComponents();
@@ -29,25 +36,17 @@ describe("LoginComponent", () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-    form = <NgForm>{
-      value: {
-        email: "test@tasksimple.com",
-        password: "testing",
-      },
-    };
-  });
-
-  it("should create", () => {
-    expect(component).toBeTruthy();
   });
 
   it(
     "should tell Router to navigate to '/user/dashboard' when is login successful",
     waitForAsync(() => {
-      auth.signInWithEmailAndPassword.and.returnValue(Promise.resolve());
+      angularFireAuthMock.signInWithEmailAndPassword.and.returnValue(
+        Promise.resolve()
+      );
       component.login(form);
       fixture.whenStable().then(() => {
-        expect(router.navigate as jasmine.Spy).toHaveBeenCalledWith([
+        expect(routerMock.navigate as jasmine.Spy).toHaveBeenCalledWith([
           "/user/dashboard",
         ]);
       });
@@ -57,8 +56,7 @@ describe("LoginComponent", () => {
   it(
     "should display an error message when login is unsuccessful",
     waitForAsync(() => {
-      expect(fixture.nativeElement.querySelector("div.alert")).toBeNull();
-      auth.signInWithEmailAndPassword.and.returnValue(
+      angularFireAuthMock.signInWithEmailAndPassword.and.returnValue(
         Promise.reject({ message: "Error" })
       );
       component.login(form);
