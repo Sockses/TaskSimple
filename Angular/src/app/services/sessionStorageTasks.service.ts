5a5707cd371da1861b5d019998146fc2e7457551
Aaron Porter <aaron.w.porter@gmail.com>
2021-06-25 07:22:18 -0700

    Create initial session task service

---
+++ Angular/src/app/services/sessionStorageTasks.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from "@angular/core";
+
+import { SessionTask } from "../models/SessionTask.model";
+
+@Injectable({ providedIn: "root" })
+export class SessionStorageTasksService {
+  tasks: SessionTask[] = [];
+  exampleAdded = false;
+
+  constructor() {}
+
+  getTasks() {
+    let tasksJson = window.sessionStorage.getItem("TaskSimpleTasks");
+
+    // add an example - just the once - if no session tasks found
+    if (tasksJson == null && !this.exampleAdded) {
+      this.addTask("An example task!");
+      this.exampleAdded = true;
+      return this.tasks;
+    }
+
+    this.tasks = this.parseTasks(tasksJson);
+    return this.tasks;
+  }
+
+  addTask(taskTitle: string) {
+    this.tasks.push(new SessionTask(taskTitle, Date(), Date(), false));
+    window.sessionStorage.setItem(
+      "TaskSimpleTasks",
+      JSON.stringify(this.tasks)
+    );
+  }
+
+  deleteTask() {
+    // TODO: Determine unique key (or add one) for update/delete actions
+  }
+
+  updateTask() {
+    // TODO: Determine unique key (or add one) for update/delete actions
+  }
+
+  checkSessionStorageAccess() {
+    return window.sessionStorage ? true : false;
+  }
+
+  parseTasks(tasksJson: string): SessionTask[] {
+    return JSON.parse(tasksJson);
+  }
+}
