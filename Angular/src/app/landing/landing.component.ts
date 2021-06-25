5b60ccfe0cc025d4a5c39565786d70d4b13d4fa3
Aaron Porter <aaron.w.porter@gmail.com>
2021-06-25 07:29:05 -0700

    Add checks for session storage and session tasks

--- Angular/src/app/landing/landing.component.ts
+++ Angular/src/app/landing/landing.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { SessionTask } from "../models/SessionTask.model";
+import { SessionStorageTasksService } from "../services/sessionStorageTasks.service";
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  selector: "app-landing",
+  templateUrl: "./landing.component.html",
+  styleUrls: ["./landing.component.scss"],
 })
 export class LandingComponent implements OnInit {
+  sessionStorageAccess = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private sessionStorageTaskService: SessionStorageTasksService) {
+    this.sessionStorageAccess =
+      this.sessionStorageTaskService.checkSessionStorageAccess();
   }
 
+  tasks: SessionTask[];
+
+  ngOnInit(): void {
+    this.loadSessionStorageTasks();
+  }
+
+  loadSessionStorageTasks() {
+    this.tasks = this.sessionStorageTaskService.getTasks();
+  }
 }
